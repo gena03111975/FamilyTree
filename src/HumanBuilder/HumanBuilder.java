2ce8b0171f9fea2144a5ffa56bb8f6e838e80610
gena <gepota1@.com>
2024-07-06 20:23:05 +0300

    добавили src/Writer/Writable.java

--- src/HumanBuilder/HumanBuilder.java
+++ src/HumanBuilder/HumanBuilder.java
@@ -1,2 +1,22 @@
-package HumanBuilder;public class HumanBuilder {
+package HumanBuilder;
+import Human.Gender;
+import Human.Human;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class HumanBuilder implements Serializable {
+    private int id;
+
+    public Human build(String name, Gender gender, LocalDate dateOfBirth){
+        return new Human(id++, name, gender, dateOfBirth);
+    };
+
+    public Human build(String name, Gender gender, LocalDate dateOfBirth, Human father, Human mother){
+        return new Human(id++, name, gender, dateOfBirth, father, mother);
+    };
+
+    public int gethumanId() {
+        return this.id;
+    }
 }
