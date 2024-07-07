4439b6ce7f5fce26415ee1fba0fee6990718e87e
gena <gepota1@.com>
2024-07-07 22:10:13 +0300

    добавили src/Dogs/DogsIterator.java

--- src/Dogs/DogsIterator.java
+++ src/Dogs/DogsIterator.java
@@ -1,2 +1,30 @@
-package Dogs;public class DogsIterator {
+package Dogs;
+
+import FamilyTree.FamilyTree;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class DogsIterator implements Iterator<Dog> {
+    private int currentId;
+    private List<Dog> listOfFamily;
+
+    public DogsIterator(FamilyTree familyTree) {
+        this.listOfFamily = familyTree.getListOfFamily();
+        this.currentId=0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentId < listOfFamily.size();
+    }
+
+    @Override
+    public Dog next() {
+        if (!hasNext()){
+            throw new NoSuchElementException();
+        }
+        return listOfFamily.get(currentId++);
+    }
 }
