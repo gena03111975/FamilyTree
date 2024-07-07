d3f01e70b2631fe92a0b4c890dfa39ce8402042e
gena <gepota1@.com>
2024-07-07 22:07:45 +0300

    добавили  src/FamilyTree/FamilyMember.java

--- src/FamilyTree/FamilyMember.java
+++ src/FamilyTree/FamilyMember.java
@@ -1,2 +1,16 @@
-package FamilyTree;public class FamilyMember {
+package FamilyTree;
+
+import java.util.List;
+
+public interface FamilyMember<T extends FamilyMember<T>> {
+
+    List<T> getChildrensList();
+
+    void addParentstoFamilyMember(T parent);
+
+    List<T> getParentsofFamilyMember();
+
+    void addChildrentoFamilyMember(T child);
+
+    Integer getId();
 }
