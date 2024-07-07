6ab9a4e766256839ef6d7d758b3ec25036fbeebc
gena <gepota1@.com>
2024-07-07 22:11:23 +0300

    добавили src/Dogs/DogsComparatorbyAmountOfPuppies.java

--- src/Dogs/DogsComparatorbyAmountOfPuppies.java
+++ src/Dogs/DogsComparatorbyAmountOfPuppies.java
@@ -1,2 +1,11 @@
-package Dogs;public class DogsComparatorbyAmountOfPuppies {
+package Dogs;
+
+import java.util.Comparator;
+
+public class DogsComparatorbyAmountOfPuppies implements Comparator<Dog> {
+
+    @Override
+    public int compare(Dog o1, Dog o2) {
+        return o1.getDogsPuppiesList().size()-o2.getDogsPuppiesList().size();
+    }
 }
